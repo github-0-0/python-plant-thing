c9c4ae1cdc1c6d04cc30246738a9e2c509057088
GitHub <74324178+github-0-0@users.noreply.github.com>
2024-09-13 12:28:18 -0700

    Create thing.py

---
+++ thing.py
@@ -0,0 +1,16 @@
+import random
+def tile(type,id,water,energy,direction):
+  return {
+    "type":type,
+    "id":id,
+    "water":water,
+    "energy":energy,
+    "direction":direction
+  }
+grid = []
+def creategrid(width,height):
+  for i in range(height):
+    grid.append([])
+    for j in range(width):
+      grid[i].append(tile(-1,-1,10,0,0))
+print(grid(10,10))
